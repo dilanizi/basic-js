e56204db1551c8c5542668af138c2c8421b63d0f
Ernest Yushkiavichus <kiberturist95@gmail.com>
2023-04-16 12:51:24 +0300

    d

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,10 +18,15 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+
+  function dateSample(sampleActivity) {
+    throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
 module.exports = {
   dateSample
 };
+}
--- src/what-season.js
+++ src/what-season.js
@@ -12,9 +12,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  
+    // Проверяем, что входной аргумент является объектом типа Date
+    if (!(date instanceof Date) || isNaN(date)) {
+      throw new Error("Invalid date object");
+    }
+
+    // Получаем месяц из объекта Date (от 0 до 11)
+    const month = date.getMonth();
+
+    // Определяем сезон в зависимости от месяца
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return "Winter";
+      case 2:
+      case 3:
+      case 4:
+        return "Spring";
+      case 5:
+      case 6:
+      case 7:
+        return "Summer";
+      case 8:
+      case 9:
+      case 10:
+        return "Autumn";
+      default:
+        throw new Error("Invalid date");
+    }
+  }
+
+
 
 module.exports = {
   getSeason
